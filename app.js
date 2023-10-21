9ad7f0686b8289c1347ad76a00ec0846be4391cc
Bravin <onsasebravin853@gmail.com>
2023-10-21 16:45:56 +0300

    fetch function

--- app.js
+++ app.js
@@ -5,4 +5,11 @@ document.addEventListener(`DOMContentLoaded`, () => {
 
     let currentFilm; // Store currently displayed film data
     let availableTickets; // Number of available tickets
+
+    //fetch movie data from API endpoint
+    fetch('http://localhost:3000/films')
+      .then(response => response.json())
+      .then(films => {
+        
+      })
 })
