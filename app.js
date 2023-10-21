1a2b82207d49a13d24ab5956d6961fdd20bbc5ca
Bravin <onsasebravin853@gmail.com>
2023-10-21 16:34:44 +0300

    JS varaiable assignment

---
+++ app.js
@@ -0,0 +1,8 @@
+document.addEventListener(`DOMContentLoaded`, () => {
+    const filmsList = document.getElementById('films');
+    const filmDetails = document.getElementById('film-details');
+    const buyTicketButton =document.getElementById('buy-ticket');
+
+    let currentFilm; // Store currently displayed film data
+    let availableTickets; // Number of available tickets
+})
